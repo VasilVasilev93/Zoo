b4445e9e5ddaa6de39802ea6a45872d065461e83
svetlio <svetlio1994@gmail.com>
2014-11-03 20:04:25 +0200

    Animal class

---
+++ Animal.py
@@ -0,0 +1,29 @@
+class Animal:
+    INITIAL_CHANCE_TO_DIE = 1
+
+    def __init__(self, name, age, gender, weight, species, life_expectancy, food_type, gestation_period, newborn_weight, average_weight, weight_age_ratio, food_weight_ratio):
+        self.is_alive = True
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.weight = weight
+        self.species = species
+        self.life_expectancy = life_expectancy
+        self.food_type = food_type
+        self.gestation_period = gestation_period
+        self.newborn_weight = newborn_weight
+        self.average_weight = average_weight
+        self.weight_age_ratio = weight_age_ratio
+        self.food_weight_ratio = food_weight_ratio
+        self.chance_to_die = self.INITIAL_CHANCE_TO_DIE
+
+    def grow(self):
+        self.age += 1
+        self.weight += 1
+        self.chance_to_die = self.age / self.life_expectancy
+
+    def eat(self, food_weight):
+        self.weight += food_weight
+
+
+
---
+++ Animal_test.py
@@ -0,0 +1,42 @@
+import unittest
+from Animal import Animal
+
+
+class TestAnimal(unittest.TestCase):
+
+    def setUp(self):
+        self.animal = Animal("name", 18, "male", 87, "species", 10, "carnivore", 9, 20, 100, 200, 20)
+
+    def test_animal_init(self):
+        self.assertEqual(self.animal.name, "name")
+        self.assertEqual(self.animal.age, 18)
+        self.assertEqual(self.animal.gender, "male")
+        self.assertEqual(self.animal.weight, 87)
+        self.assertEqual(self.animal.species, "species")
+        self.assertEqual(self.animal.life_expectancy, 10)
+        self.assertEqual(self.animal.food_type, "carnivore")
+        self.assertEqual(self.animal.gestation_period, 9)
+        self.assertEqual(self.animal.newborn_weight, 20)
+        self.assertEqual(self.animal.average_weight, 100)
+        self.assertEqual(self.animal.weight_age_ratio, 200)
+        self.assertEqual(self.animal.food_weight_ratio, 20)
+        self.assertTrue(self.animal.is_alive)
+
+    def test_animal_grow(self):
+        self.animal.grow()
+        self.assertEqual(self.animal.age, 19)
+        self.assertEqual(self.animal.weight, 88)
+
+    def test_animal_eat(self):
+        self.animal.eat(10)
+        self.assertEqual(self.animal.weight, 97)
+
+    def test_animal_die(self):
+        results = set()
+        for i in range(1000):
+            self.animal.grow()
+            results.add(self.animal.is_alive)
+        self.assertTrue(True in results)
+
+if __name__ == '__main__':
+    unittest.main()
